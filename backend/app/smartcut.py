a8eb83333f0a75938d71c2a0395296bb7da0d57d
Andrey Germanov <andrey@germanov.dev>
2023-09-26 19:30:00 +0300

    Integrate Kafka messaging to backend

--- backend/app/smartcut.py
+++ backend/app/smartcut.py
@@ -1,14 +1,14 @@
 import os
 import uvicorn
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException
-from plugins import get_plugins_list, run_update_plugins_list, stop_update_plugins_list, get_result
+from plugins import manager
 from storage import save_file
 
 app = FastAPI()
 
 
 def main():
-    run_update_plugins_list()
+    manager.run()
     uvicorn.run(app, host="0.0.0.0", port=8080)
 
 
@@ -17,48 +17,55 @@ async def get_plugins():
     """
     Возвращает фронтенду список активных плагинов
     """
-    return get_plugins_list()
+    return manager.get_plugins()
 
 
 @app.post("/api/upload_zip")
 async def upload_zip(
-        uid: str = Form(),
+        user_request_uid: str = Form(),
         video_file_name: str = Form(),
-        zip: UploadFile = File()):
+        plugin_name: str = Form(),
+        zip: UploadFile = File(),
+        is_last: bool = Form()):
     """
     Принимает zip-файл с кадрами из фронтенда и сохраняет его в хранилище
-    :param uid: Идентификатор запроса
+    и оповещаяет плагин о загрузке файла, отправляя сообщение в Kafka
+    :param user_request_uid: Идентификатор запроса
     :param video_file_name: Имя видео-файла
-    :param zip: Тело zip-архива
-    :return:
-    """
-    save_file(os.path.join(uid, video_file_name), zip.filename, zip.file)
-    return {"status": "ok"}
-
-
-@app.get("/api/post_video_to_plugin/{uid}/{plugin_name}/{video_file_name}")
-async def post_video_to_plugin(uid, plugin_name, video_file_name):
-    """
-    Ставит задачу плагину начать обработку загруженного видео
-    (Пока ничего не делает)
-    :param uid: Идентификатор запроса
     :param plugin_name: Имя плагина
-    :param video_file_name: Имя видео-файла
+    :param zip: Тело zip-архива
+    :param is_last: Является ли данный zip-файл последним для данного видео-файла
     :return:
     """
-    return {"uid": uid, "plugin_name": plugin_name, "video_file_name": video_file_name}
+    path = os.path.join(user_request_uid, video_file_name)
+    try:
+        save_file(path, zip.filename, zip.file)
+        if is_last:
+            manager.send_message(plugin_name, {
+                "status": "uploaded",
+                "user_id": "user_id1",
+                "zipped_chunks_path": path
+            })
+        else:
+            manager.send_message(plugin_name, {
+                "status": "in-progress",
+                "user_id": "user_id1",
+                "last_zipped_chunk_path": path + "/" + zip.filename
+             })
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return {"status": "ok"}
 
 
 @app.get("/api/get_video_intervals/{uid}")
 async def get_video_intervals(uid):
     """
-    Запрашивает результат обработки для указанного идентификатора запроса
-    (Пока возвращает статические данные)
+    Запрашивает результат обработки видео для указанного идентификатора запроса
     :param uid: Идентификатор запроса
     :return:
     """
-    result = get_result(uid)
-    if result == None:
+    result = manager.get_result(uid)
+    if result is None:
         raise HTTPException(status_code=404, detail="Item not found")
     else:
         return result
@@ -70,7 +77,7 @@ def shutdown_event():
     При завершении работы FastAPI, завершает все остальные фоновые процессы
     :return:
     """
-    stop_update_plugins_list()
+    manager.stop()
 
 
 main()
