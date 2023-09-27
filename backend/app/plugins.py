33c2fc2546a29457abca53b38cb699b526e914c4
Andrey Germanov <andrey@germanov.dev>
2023-09-27 10:48:35 +0300

    Fix receiving init message from "start-plugins" topic when running from Docker container.

--- backend/app/plugins.py
+++ backend/app/plugins.py
@@ -23,20 +23,23 @@ class PluginsManager:
         # Ключ - user_request_id
         self.results = {}
 
-        # Продюсер Kafka для отправки сообщений плагинам через Kafka
+        # Продюсер Kafka для отправки сообщений плагинам
         self.kafka_producer = KafkaProducer(
             bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVER"),
             value_serializer=lambda x: dumps(x).encode("utf-8")
         )
 
-        # Консьюмер Kafka для получения сообщений от плагинов
-        self.kafka_consumer = KafkaConsumer(
+        # Консьюмеры Kafka для получения сообщений от плагинов
+        self.kafka_start_plugins_consumer = KafkaConsumer("start-plugins",
             bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVER"),
             value_deserializer=lambda x: loads(x.decode("utf-8")),
             consumer_timeout_ms=int(os.getenv("KAFKA_POLL_PERIOD"))*1000
         )
-        # Подписываемся сразу на все нужные топики
-        self.kafka_consumer.subscribe(["start-plugins", "processed-files"])
+        self.kafka_processed_files_consumer = KafkaConsumer("processed-files",
+            bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVER"),
+            value_deserializer=lambda x: loads(x.decode("utf-8")),
+            consumer_timeout_ms=int(os.getenv("KAFKA_POLL_PERIOD"))*1000
+        )
 
         # Максимальное время хранения результата запроса пользователя в кэше (секунды)
         self.RESULT_TIME_TO_LIVE = int(os.getenv("RESULT_TIME_TO_LIVE"))
@@ -47,8 +50,8 @@ class PluginsManager:
         чтения сообщений из Kafka
         :return:
         """
-        thread = Thread(target=self.__kafka_poll)
-        thread.start()
+        Thread(target=self.__kafka_poll_start_plugins).start()
+        Thread(target=self.__kafka_poll_processed_files).start()
 
     def stop(self):
         """
@@ -85,19 +88,33 @@ class PluginsManager:
         else:
             return None
 
-    def __kafka_poll(self):
+    def __kafka_poll_start_plugins(self):
         """
-        Функция обновляет список плагинов и результатов запросов пользователей из Kafka
+        Функция обновляет список плагинов из Kafka
         :return:
         """
         while True:
             if stop_flag:
                 break
             try:
-                for message in self.kafka_consumer:
-                    if message.topic == "start-plugins" and message.value.get("plugin_name"):
+                for message in self.kafka_start_plugins_consumer:
+                    if message.value.get("plugin_name"):
                         self.plugins[message.value["plugin_name"]] = message.value
-                    elif message.topic == "processed-files" and message.value.get("user_request_uid"):
+                self.__clean_results()
+            except Exception as e:
+                logging.error(e)
+
+    def __kafka_poll_processed_files(self):
+        """
+        Функция обновляет список результатов запросов пользователей из Kafka
+        :return:
+        """
+        while True:
+            if stop_flag:
+                break
+            try:
+                for message in self.kafka_processed_files_consumer:
+                    if message.value.get("user_request_uid"):
                         self.results[message.value["user_request_uid"]] = message.value
                         self.results[message.value["user_request_uid"]]["time"] = time.time()
                 self.__clean_results()
